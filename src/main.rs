88d3ced6cba34fc51161278949c2511d68c5e44e
wasabina67 <wasabina67@gmail.com>
2025-10-15 07:32:24 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "todo-cli")]
+#[command(about = "todo-cli", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Add
+    Add { description: String },
+    /// List
+    List,
+    /// Complete
+    Complete { id: usize },
+    /// Remove
+    Remove { id: usize },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Add { description } => {
+            println!("Add: {}", description);
+        }
+        Commands::List => {
+            println!("List");
+        }
+        Commands::Complete { id } => {
+            println!("Complete: id {}", id);
+        }
+        Commands::Remove { id } => {
+            println!("Remove: id {}", id);
+        }
+    }
 }
